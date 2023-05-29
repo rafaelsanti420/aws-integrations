c1eaeb9b042083c867dba56c2929574532e712f9
rafaelsanti420 <rafaelsanti420@gmail.com>
2023-05-29 15:47:59 +0300

    added aws sqs learning example

--- src/aws/config.ts
+++ src/aws/config.ts
@@ -1,5 +1,5 @@
 export class ApplicationConfiguration {
   static readonly region = process.env.REGION ?? 'us-east-1'
-  static readonly accessKeyId = process.env.ACCESSKEYID ?? ''
+  static readonly accessKeyId = process.env.ACCESSKEYID ?? ' '
   static readonly secretAccessKey = process.env.SECRETACCESKEY ?? ''
 }
